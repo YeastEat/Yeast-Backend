8734ef6dd25f107a73fffebca27a433abd342013
eeuunn <haspring@dsm.hs.kr>
2024-06-11 15:40:26 +0900

    set : project setting

---
+++ src/main/kotlin/com/example/yeastbackend/YeastBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yeastbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YeastBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<YeastBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/yeastbackend/YeastBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.yeastbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class YeastBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
